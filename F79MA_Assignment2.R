25cb8783c9791c5bed422efee8b6f825e6d6107d
mod2001 <mod2001@hw.ac.uk>
2025-11-22 15:26:01 +0400

    Added: Report

--- F79MA_Assignment2.R
+++ F79MA_Assignment2.R
@@ -27,27 +27,40 @@ Quantity1 = k / x_bar
 
 # Deriving the posterior density for p and finding its mean
 n = 1000
-s = sum(My_Data - k)
+S = sum(My_Data - k)
 alpha_post = n * k
-beta_post = s + 0.5
+beta_post = S + 0.5
 posterior_samples = rbeta(n, shape1 = alpha_post, shape2 = beta_post)
-Quantity2 = mean(posterior_samples)
+Quantity2 = alpha_post / (alpha_post + beta_post)
 
-# Deriving the prior distribution using an approximation
-prior_samples = rbeta(n, shape1 = 0.5, shape2 = 0.5)
+# Defining the Jeffrey's prior
+p_vals = seq(0.001, 0.999, length.out = n)
+jeff_prior = 1 / (p_vals * sqrt(1 - p_vals))
 
 # Put samples into a data frame for ggplot
-df <- rbind(
-  data.frame(p = prior_samples, type = "Jeffreys Prior"),
-  data.frame(p = posterior_samples, type = "Posterior")
+jeff_df = data.frame(
+  p = p_vals,
+  value = jeff_prior,
+  type = "Jeffrey's Prior"
 )
+post_df = data.frame(
+  p = posterior_samples,
+  type = "Posterior"
+  
+)
+ggplot() +
+  geom_line(data = jeff_df, aes(x = p, y = value, colour = "Jeffreys Prior (Improper)"), linewidth = 1) +
+  geom_density(data = post_df, aes(x = p, colour = "Posterior"), adjust = 2, linewidth = 1) +
+  coord_cartesian(xlim = c(0, 0.25)) +
+  labs(
+    title = "Jeffrey's Prior vs. Posterior",
+    x = "p",
+    y = "Density"
+  ) +
+  scale_colour_manual(values = c("Jeffreys Prior (Improper)" = "blue", "Posterior" = "red")) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(df, aes(x = p, fill = type)) +
-  geom_density(alpha = 0.4, adjust = 3) +
-  xlim(0, 0.3) +
-  labs(title = "Jeffreys Prior vs Posterior (Negative Binomial)",
-       x = "p", y = "Density") +
-  theme_minimal()
 
 # Posterior predictive distribution and using this
 # to find the value of pi(Z = 5|x)
@@ -56,11 +69,12 @@ z = 5
 binomial_coef = choose(z - 1, k - 1)
 Quantity3 = binomial_coef * exp(lbeta(alpha_post + k, beta_post + z - k) - lbeta(alpha_post, beta_post))
 
-
 # Simulating 10000 realizations from the posterior
 # predictive distribution
+
 realisations = 10000
 posterior_predictive_samples = rnbinom(realisations, size = k, prob = posterior_samples)
 
 hist(posterior_predictive_samples, breaks = 30, freq = F,main = "Posterior Predictive Distribution",
-     xlab = "Count", ylab = "Frequency", col = "lightblue")
+     xlab = "Count", col = "lightblue",)
+hist(Full_Data, breaks = 30, freq = F, main = "Histogram of Full_Data", col = "lightgreen", add = T)
